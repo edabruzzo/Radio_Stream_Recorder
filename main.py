08db8b3b0e94e68a72f8eeb8ab4ca2e1160be94b
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2021-10-17 19:41:37 -0300

    Adiciona streamings

--- main.py
+++ main.py
@@ -12,43 +12,47 @@ https://stackoverflow.com/questions/2046663/record-output-sound-in-python
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import datetime
+from datetime import time
 
-import pyaudio, wave, sys
 
-chunk = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = sys.argv[1]
+def read_sound_device_output():
+    import pyaudio, wave, sys
 
-p = pyaudio.PyAudio()
-channel_map = (0, 1)
+    chunk = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    RECORD_SECONDS = 5
+    WAVE_OUTPUT_FILENAME = sys.argv[1]
 
-stream_info = pyaudio.PaMacCoreStreamInfo(
-    flags = pyaudio.PaMacCoreStreamInfo.paMacCorePlayNice,
-    channel_map = channel_map)
+    p = pyaudio.PyAudio()
+    channel_map = (0, 1)
 
-stream = p.open(format = FORMAT,
-                rate = RATE,
-                input = True,
-                input_host_api_specific_stream_info = stream_info,
-                channels = CHANNELS)
+    stream_info = pyaudio.PaMacCoreStreamInfo(
+        flags = pyaudio.PaMacCoreStreamInfo.paMacCorePlayNice,
+        channel_map = channel_map)
 
-all = []
-for i in range(0, RATE / chunk * RECORD_SECONDS):
-        data = stream.read(chunk)
-        all.append(data)
-stream.close()
-p.terminate()
+    stream = p.open(format = FORMAT,
+                    rate = RATE,
+                    input = True,
+                    input_host_api_specific_stream_info = stream_info,
+                    channels = CHANNELS)
 
-data = ''.join(all)
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(data)
-wf.close()
+    all = []
+    for i in range(0, RATE / chunk * RECORD_SECONDS):
+            data = stream.read(chunk)
+            all.append(data)
+    stream.close()
+    p.terminate()
+
+    data = ''.join(all)
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(data)
+    wf.close()
 
 
 
@@ -60,8 +64,19 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+# https://stackoverflow.com/questions/10048249/how-do-i-determine-if-current-time-is-within-a-specified-range-using-pythons-da
+def esta_dentro_do_horario_limite(begin_time, end_time, check_time=None):
+    check_time = check_time or datetime.datetime.now().time()
+    return check_time >= begin_time or check_time <= end_time
+
+dentro_do_horario_limite = esta_dentro_do_horario_limite(time(14,00), time(19,00))
+
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    hoje = datetime.datetime.today().isoweekday() # "Return day of the week, where Monday == 1 ... Sunday == 7."
+    print(hoje)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- record_internet_radio_streaming.py
+++ record_internet_radio_streaming.py
@@ -6,21 +6,82 @@ https://www.svnlabs.com/blogs/record-live-streaming-radio-to-mp3/
 https://stackoverflow.com/questions/4247248/record-streaming-and-saving-internet-radio-in-python
 https://www.svnlabs.com/blogs/record-live-streaming-radio-to-mp3/
 http://manpages.ubuntu.com/manpages/impish/en/man1/streamripper.1.html
+https://stackoverflow.com/questions/16717930/how-to-run-crontab-job-every-week-on-sunday
 
+
+https://stackoverflow.com/questions/10048249/how-do-i-determine-if-current-time-is-within-a-specified-range-using-pythons-da
+https://docs.python.org/pt-br/3/library/datetime.html
+
+https://www.py4u.net/discuss/169119
 '''
 import datetime
+from datetime import time
 import os.path
 
-URL_REQUEST_89FM = 'https://21933.live.streamtheworld.com/RADIO_89FM_ADP.aac?dist=site-89fm'
+def executa_gravador_streamin(LIMITAR_TARDES_DOMINGO=True):
 
-agora = datetime.datetime.now()
-hora_formatada = agora.strftime('%d_%m_%Y_%H_hs_%M_min_%S_seg')
+    URL_REQUEST_89FM = 'https://21933.live.streamtheworld.com/RADIO_89FM_ADP.aac?dist=site-89fm'
 
-arquivo_mp3 = os.path.join(os.path.dirname(__file__), '/streams/89FM_{}.mp3'.format(hora_formatada))
+    agora = datetime.datetime.now()
+    hora_formatada = agora.strftime('%d_%m_%Y_%H_hs_%M_min_%S_seg')
 
-comando_curl = 'curl -sS -o {} –max-time 1800 {}'.format(arquivo_mp3, URL_REQUEST_89FM)
-comando_streamripper = 'streamripper {} -d ./streams -l 10800 -a {}'.format(URL_REQUEST_89FM, arquivo_mp3)
+    arquivo_mp3 = os.path.join(os.path.dirname(__file__), '/streams/89FM_{}.mp3'.format(hora_formatada))
 
-os.system('sudo apt-get install streamripper')
+    comando_curl = 'curl -sS -o {} –max-time 1800 {}'.format(arquivo_mp3, URL_REQUEST_89FM)
+    comando_streamripper = 'streamripper {} -d ./streams -l 10800 -a {}'.format(URL_REQUEST_89FM, arquivo_mp3)
 
-os.system(comando_streamripper)
+    if LIMITAR_TARDES_DOMINGO:
+        hoje = datetime.datetime.today().isoweekday()  # "Return day of the week, where Monday == 1 ... Sunday == 7."
+        if hoje == 7:
+            try:
+                os.system(comando_streamripper)
+            except:
+                os.system('sudo apt-get install streamripper')
+
+
+def esta_dentro_do_horario_limite(begin_time, end_time, check_time=None):
+    check_time = check_time or datetime.now().time()
+    return check_time >= begin_time or check_time <= end_time
+
+
+def argumentos_linha_comando():
+    '''https://cadernodelaboratorio.com.br/python-3-processando-argumentos-da-linha-de-comando/'''
+    import sys
+    import argparse
+    parser = argparse.ArgumentParser(description='Determina se há restrição de dia e horário')
+
+    parser.add_argument('--horario_livre', type=bool, default=False,
+                        help="""
+                            Permite a execução em qualquer horário
+                         
+                                """)
+    parser.add_argument('--tardes_domingo', type=bool, default=False,
+                        help="""
+                                 Execução apenas domingo
+                            """)
+
+    args = parser.parse_args()
+
+    TARDES_DOMINGO = args.tardes_domingo
+    HORARIO_LIVRE = args.horario_livre
+
+    return TARDES_DOMINGO, HORARIO_LIVRE
+
+
+if __name__ == "__main__":
+
+    LIMITAR_A_TARDES_DOMINGO, HORARIO_LIVRE = argumentos_linha_comando()
+
+
+    if HORARIO_LIVRE:
+        print('Aguardando interrupção manual do programa')
+        executa_gravador_streamin()
+
+    else:
+
+        while True:
+
+            dentro_do_horario_limite = esta_dentro_do_horario_limite(time(14, 00), time(20, 35))
+
+            if not dentro_do_horario_limite:
+                exit()
